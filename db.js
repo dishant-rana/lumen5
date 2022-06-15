fa3efb922cefb85585d0622b8251422d9c5b21be
Huzaifa Sheikh <99542545+huzaifa621@users.noreply.github.com>
2022-06-15 13:07:42 +0530

    folder optimization

--- db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-const connection = mongoose.connect('mongodb+srv://huzaifa_621:Huzaifa_19_%23_mongoDB_Atlass_&@cluster0.1bzys.mongodb.net/?retryWrites=true&w=majority')
-
-module.exports = connection;
